34660c0c019af9ce979e60bf192d8210a2abe00c
kevin Leo <kevmanyonge@gmail.com>
2023-11-24 11:40:38 +0300

    Online voting

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/voting/ussd/', [\App\Http\Controllers\VoteController::class, 'ussd'])->name('api.voting.ussd');
