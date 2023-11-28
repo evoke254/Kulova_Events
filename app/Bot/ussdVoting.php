530136708efa111f02eba49e7549a27fe5116e12
kevin Leo <kevmanyonge@gmail.com>
2023-11-29 01:48:33 +0300

    Set USSD

--- app/Bot/ussdVoting.php
+++ app/Bot/ussdVoting.php
@@ -109,6 +109,7 @@ class ussdVoting extends Conversation
                 }
                 break;
             }
+                break;
         }
 
         if ($countBallot >0) {
@@ -205,11 +206,11 @@ class ussdVoting extends Conversation
         $qstn = "CON POSITIONS: \n ".   $opt ."\n 1 : Confirm\n 2 : Cancel and Start";
         $this->ask($qstn, function(Answer $answer) use ($opt, $pstnKey) {
             $ans = (int)$answer->getText();
-            if ($ans == 2){
+            if ($ans == 1){
                 $this->say('END Vote cast. Thank you.');
             } else {
                 $this->deleteVote();
-                        $this->say('END Cancelled by user.');
+                        $this->say('END Cancelled by user. Dial *544# to try again');
             }
 
         });
