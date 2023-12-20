f438d661ae5c5ef790d5b44beb1f7e3ae7fed903
kevin Leo <kevmanyonge@gmail.com>
2023-12-20 18:48:27 +0300

    Finalize Whatsapp voting

--- app/Bot/Driver/whatsapp.php
+++ app/Bot/Driver/whatsapp.php
@@ -108,13 +108,16 @@ class whatsapp extends HttpDriver implements VerifiesService
      */
     public function matchesRequest()
     {
+
         if (isset($this->event['messages'][0]['id'])){
             $db_log = BotLogs::where('message_id', $this->event['messages'][0]['id'])->first();
+            Log::info(json_encode($db_log));
             if ($db_log){
                 die();
             }
         }
 
+        Log::info('we are past this');
 
         return (isset($this->event['messages'][0]) && isset($this->event['messages'][0]['from']));
     }
@@ -364,7 +367,6 @@ class whatsapp extends HttpDriver implements VerifiesService
                 $parameters['text']['body'] = $message->getText();
             }
         }
-
 //        $parameters['access_token'] = $this->config->get('token');
 
         return $parameters;
